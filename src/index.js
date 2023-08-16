e0c9af93f0590f4657bdb93542be2dba08bf49c2
Vasjan Çupri <vasjancupri04@gmail.com>
2023-08-16 18:38:34 +0300

    Added the home page and some recipe data

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import "./style.css"
+import data from "./recipes.json"
 
 function nav() {
   const navContainer = document.createElement("nav")
-  const homeAnchor = document.createElement('a')
-  const menuAnchor = document.createElement('a')
-  const contactAnchor = document.createElement('a')
+  const homeAnchor = document.createElement("a")
+  const menuAnchor = document.createElement("a")
+  const contactAnchor = document.createElement("a")
 
   homeAnchor.textContent = "Home"
+  homeAnchor.setAttribute("href", "#home")
   menuAnchor.textContent = "Menu"
+  menuAnchor.setAttribute("href", "#menu")
   contactAnchor.textContent = "Contact"
+  contactAnchor.setAttribute("href", "#contact")
 
   navContainer.appendChild(homeAnchor)
   navContainer.appendChild(menuAnchor)
@@ -16,4 +20,24 @@ function nav() {
   return navContainer
 }
 
+function home() {
+  const homeContainer = document.createElement("div")
+  homeContainer.setAttribute("id", "home")
+
+  const ctaText = document.createElement("h2")
+  ctaText.textContent = "Welcome to Ina's Spice & Savor"
+
+  const menuBtn = document.createElement("a")
+  menuBtn.textContent = "Check our Menu"
+  menuBtn.setAttribute("href", "#menu")
+
+  homeContainer.appendChild(ctaText)
+  homeContainer.appendChild(menuBtn)
+
+  return homeContainer
+}
+
+console.log(data)
+
 document.body.appendChild(nav())
+document.body.appendChild(home())
